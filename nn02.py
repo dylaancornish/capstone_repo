7a1a22ca7a6b73bda20357bef9729c7d06555cea
dylaancornish <dylaanc@gmail.com>
2022-05-10 14:43:58 -0500

    deleted unused code and comments

--- nn02.py
+++ nn02.py
@@ -8,42 +8,6 @@ import torch.optim as optim
 from sklearn import preprocessing
 import os
 
-#define cnn class
-# class Net(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.conv1 = nn.Conv2d(1, 64, 3, padding=1)
-#         self.bn1 = nn.BatchNorm2d(64)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.conv2 = nn.Conv2d(64, 128, 3, padding=1)
-#         self.bn2 = nn.BatchNorm2d(128)
-#         self.conv3 = nn.Conv2d(128, 256, 3, padding=1)
-#         self.bn3 = nn.BatchNorm2d(256)
-#         self.conv4 = nn.Conv2d(256, 512, 3, padding=1)
-#         self.bn4 = nn.BatchNorm2d(512)
-#         self.fc1 = nn.Linear(4096, 21)
-#         # self.fc2 = nn.Linear(5120, 2560)
-#         # self.fc3 = nn.Linear(2560, 21)
-
-#     #forward propogation
-#     def forward(self, x):
-#         x = (F.relu(self.conv1(x)))
-#         x = self.bn1(x)
-#         x = F.relu(self.conv2(x))
-#         x = self.bn2(x)
-#         x = self.pool(x)
-#         x = F.relu(self.conv3(x))
-#         x = self.bn3(x)
-#         x = self.pool(x)
-#         x = F.relu(self.conv4(x))
-#         x = self.bn4(x)
-#         x = self.pool(x)
-#         x = torch.flatten(x, 1) # flatten all dimensions except batch
-#         # x = F.relu(self.fc1(x))
-#         # x = F.relu(self.fc2(x))
-#         # x = self.fc3(x)
-#         x = self.fc1(x)
-#         return x
 
 class Net(nn.Module):
     def __init__(self):
